de551b63e10af16467060c98e046ca6d28f8eba5
Michiel van Rooy <michielvanrooy@gmail.com>
2019-09-11 21:17:10 +0200

    code is work but needs some refactoring

---
+++ RaspberryLamp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RaspberryRobotGpio;
+
+namespace RaspberryLamp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly GpioController gpioController;
+
+        public HomeController(GpioController gpioController)
+        {
+            this.gpioController = gpioController;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult TurnLampOn()
+        {
+            this.gpioController.WritePin(PinValue.High);
+            return View("Index");
+        }
+
+        public IActionResult TurnLampOff()
+        {
+            this.gpioController.WritePin(PinValue.Low);
+            return View("Index");
+            
+        }
+    }
+}
---
+++ RaspberryRobotGpio/GpioController.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace RaspberryRobotGpio
+{
+    public class GpioController
+    {
+        private readonly string gpioPath = "/sys/class/gpio/";
+        private int pinNumber = 18;
+
+        public GpioController()
+        {
+            this.Init();
+        }
+
+        public void Init()
+        {
+            //Check if port is already open
+            if (Directory.Exists($"{gpioPath}gpio{pinNumber.ToString()}"))
+            {
+                return;
+            }
+
+            File.WriteAllText($"{gpioPath}export", pinNumber.ToString());
+            File.WriteAllText($"{gpioPath}gpio{pinNumber.ToString()}/direction", "out");
+        }
+
+        public void WritePin(PinValue value)
+        {
+            var valueStr = ((byte)value).ToString();
+            File.WriteAllText($"{gpioPath}gpio{pinNumber.ToString()}/value", valueStr);
+        }
+
+        public void Dispose()
+        {
+            File.WriteAllText($"{gpioPath}unexport", pinNumber.ToString());
+        }
+    }
+}
